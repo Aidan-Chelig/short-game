a585d5d01786ccde8aeb193584fdbecdc829e73d
aidan chelig <aidan.chelig@gmail.com>
2022-06-29 15:43:50 -0500

    switch example to rotating cube example

--- src/main.rs
+++ src/main.rs
@@ -1,56 +1,57 @@
 use bevy::prelude::*;
 
-/// This example shows how to configure Multi-Sample Anti-Aliasing. Setting the sample count higher
-/// will result in smoother edges, but it will also increase the cost to render those edges. The
-/// range should generally be somewhere between 1 (no multi sampling, but cheap) to 8 (crisp but
-/// expensive).
-/// Note that WGPU currently only supports 1 or 4 samples.
-/// Ultimately we plan on supporting whatever is natively supported on a given device.
-/// Check out [this issue](https://github.com/gfx-rs/wgpu/issues/1832) for more info.
+use std::f32::consts::PI;
+
+const FULL_TURN: f32 = 2.0 * PI;
+
+// Define a component to designate a rotation speed to an entity.
+#[derive(Component)]
+struct Rotatable {
+    speed: f32,
+}
+
 fn main() {
     App::new()
-        .insert_resource(Msaa { samples: 4 })
         .add_plugins(DefaultPlugins)
         .add_startup_system(setup)
-        .add_system(cycle_msaa)
+        .add_system(rotate_cube)
         .run();
 }
 
-/// set up a simple 3D scene
 fn setup(
     mut commands: Commands,
     mut meshes: ResMut<Assets<Mesh>>,
     mut materials: ResMut<Assets<StandardMaterial>>,
 ) {
-    info!("Press 'm' to toggle MSAA");
-    info!("Using 4x MSAA");
+    // Spawn a cube to rotate.
+    commands
+        .spawn_bundle(PbrBundle {
+            mesh: meshes.add(Mesh::from(shape::Cube { size: 1.0 })),
+            material: materials.add(Color::WHITE.into()),
+            transform: Transform::from_translation(Vec3::ZERO),
+            ..Default::default()
+        })
+        .insert(Rotatable { speed: 0.3 });
 
-    // cube
-    commands.spawn_bundle(PbrBundle {
-        mesh: meshes.add(Mesh::from(shape::Cube { size: 2.0 })),
-        material: materials.add(Color::rgb(0.8, 0.7, 0.6).into()),
-        ..default()
-    });
-    // light
-    commands.spawn_bundle(PointLightBundle {
-        transform: Transform::from_xyz(4.0, 8.0, 4.0),
-        ..default()
-    });
-    // camera
+    // Spawn a camera looking at the entities to show what's happening in this example.
     commands.spawn_bundle(PerspectiveCameraBundle {
-        transform: Transform::from_xyz(-3.0, 3.0, 5.0).looking_at(Vec3::ZERO, Vec3::Y),
-        ..default()
+        transform: Transform::from_xyz(0.0, 10.0, 20.0).looking_at(Vec3::ZERO, Vec3::Y),
+        ..Default::default()
+    });
+
+    // Add a light source for better 3d visibility.
+    commands.spawn_bundle(PointLightBundle {
+        transform: Transform::from_translation(Vec3::ONE * 3.0),
+        ..Default::default()
     });
 }
 
-fn cycle_msaa(input: Res<Input<KeyCode>>, mut msaa: ResMut<Msaa>) {
-    if input.just_pressed(KeyCode::M) {
-        if msaa.samples == 4 {
-            info!("Not using MSAA");
-            msaa.samples = 1;
-        } else {
-            info!("Using 4x MSAA");
-            msaa.samples = 4;
-        }
+// This system will rotate any entity in the scene with an assigned Rotatable around its z-axis.
+fn rotate_cube(mut cubes: Query<(&mut Transform, &Rotatable)>, timer: Res<Time>) {
+    for (mut transform, cube) in cubes.iter_mut() {
+        // The speed is taken as a percentage of a full 360 degree turn.
+        // The timers delta_seconds is used to smooth out the movement.
+        let rotation_change = Quat::from_rotation_y(FULL_TURN * cube.speed * timer.delta_seconds());
+        transform.rotate(rotation_change);
     }
 }
