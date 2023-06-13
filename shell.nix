f075bd24d8fcc7e332b16ea941629e4f2ecc0dfa
aidan chelig <aidan.chelig@gmail.com>
2023-06-13 17:24:24 -0500

    update shell and update ignore

--- shell.nix
+++ shell.nix
@@ -1,38 +1,46 @@
-let
-  moz_overlay = import (fetchTarball https://github.com/mozilla/nixpkgs-mozilla/archive/master.tar.gz);
-  nixpkgs = import <nixpkgs> { overlays = [ moz_overlay ]; };
-    rustChannel = nixpkgs.rustChannelOf {
-     channel = "stable";
-  };
-  rust = (rustChannel.rust.override {
-    targets = [
-      "wasm32-unknown-unknown" # required for the web-app
-    ];
-    extensions = ["rust-src" "rust-analysis"];
-  });
 
+{ pkgs ? import <nixpkgs> {} }:
+
+let
+
+  fenix = import (fetchTarball "https://github.com/nix-community/fenix/archive/main.tar.gz") { };
 in
-with nixpkgs;
+
 pkgs.mkShell {
+
   shellHook = ''export LD_LIBRARY_PATH="$LD_LIBRARY_PATH:${pkgs.lib.makeLibraryPath [
     pkgs.alsaLib
     pkgs.udev
     pkgs.vulkan-loader
   ]}"'';
 
-  buildInputs = with nixpkgs; [
+  buildInputs = with pkgs; [
+
+    (
+      with fenix;
+      combine (
+        with default; [
+          cargo
+          clippy-preview
+          latest.rust-src
+          rust-analyzer
+          rust-std
+          rustc
+          rustfmt-preview
+        ]
+      )
+    )
     cargo-edit
     cargo-watch
-    rust
+    pkg-config
+    alsaLib
+    jack2
 
     lld
     clang
 
-    # # bevy-specific deps (from https://github.com/bevyengine/bevy/blob/main/docs/linux_dependencies.md)
-    pkgconfig
     udev
     lutris
-    xlibsWrapper
     xorg.libXcursor
     xorg.libXrandr
     xorg.libXi
@@ -41,6 +49,8 @@ pkgs.mkShell {
     vulkan-loader
     vulkan-validation-layers
     libjack2
+
+    # # bevy-specific deps (from https://github.com/bevyengine/bevy/blob/main/docs/linux_dependencies.md)
   ];
 
 }
